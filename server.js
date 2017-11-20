676139566a1f16a40f27d02b243388c9d6280292
Taha Abbasi <taha@mywebnapp.com>
2017-11-20 12:45:13 -0500

    added underscore validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,13 +21,7 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -38,6 +33,10 @@ app.get('/todos/:id', function (req, res) {
 // POST /todos
 app.post('/todos', function (req, res) {
     var body = req.body;
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
 
     // add id field
     body.id = todoNextId++;
