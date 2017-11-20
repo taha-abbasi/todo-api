8c014fc009ed162de7dea85c76f88f65d3c480e9
Taha Abbasi <taha@mywebnapp.com>
2017-11-20 00:10:50 -0500

    Added POST /todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Feed the dog',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -29,8 +22,6 @@ app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo;
 
-    // Iterate of todos array. Find match.
-
     todos.forEach(function (todo) {
         if (todoId === todo.id) {
             matchedTodo = todo;
@@ -42,6 +33,19 @@ app.get('/todos/:id', function (req, res) {
     } else {
         res.status(404).send();
     }
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    // add id field
+    body.id = todoNextId++;
+
+    // push body into array
+    todos.push(body);
+
+    res.json(body);
 });
 
 app.listen(PORT, function () {
