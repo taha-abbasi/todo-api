7a34bef29a91c72890ebcabf713cda1b8ee9e33c
Taha Abbasi <taha@mywebnapp.com>
2017-11-19 23:30:44 -0500

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -25,6 +25,24 @@ app.get('/todos', function (req, res) {
 });
 
 // GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+
+    // Iterate of todos array. Find match.
+
+    todos.forEach(function (todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
